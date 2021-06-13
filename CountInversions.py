c81d5bc548ffe658349384b4c2edc58045dc7798
phase-plane <alexander.johannes.17@ucl.ac.uk>
2021-06-13 11:50:22 +0200

    Updated (wip) count inversions, upload integer text file.

--- CountInversions.py
+++ CountInversions.py
@@ -1,6 +1,16 @@
 # modify MergeSort to count the number of inversions in an array
 # sort and merge an array
-testArray = [1, 3, 5, 2, 4, 6]
+def SimpleInvCount(array):
+    """complexity O(n**2)"""
+    count = 0
+    n = len(array)
+    for l in range(0, n - 1):
+        for k in range(l + 1, n):
+            if array[l] > array[k]:
+                count += 1
+    return count
+
+# sort and count inversions
 def SortCountInv(array):
     """complexity O(n.log(n))"""
     # base case
@@ -11,38 +21,43 @@ def SortCountInv(array):
         midpoint = len(array) // 2
         leftArray = array[:midpoint]
         rightArray = array[midpoint:]
-        C, leftInv = SortCountInv(leftArray)
-        D, rightInv = SortCountInv(rightArray)
-        B, splitInv = MergeCountSplitInv(C, D)
-        return B, leftInv + rightInv + splitInv
+        # left, right = MergeSort(left_array), MergeSort(right_array)
+        # return merge(left, right, array.copy())
+        sortedLeft, leftInv = SortCountInv(leftArray)
+        sortedRight, rightInv = SortCountInv(rightArray)
+        mergedResult, splitInv = MergeCountSplitInv(sortedLeft, sortedRight, array.copy())
+        return mergedResult, leftInv + rightInv + splitInv
 
-      # left, right = MergeSort(left_array), MergeSort(right_array)
-      # return merge(left, right, array.copy())
 
-# merge subroutine
-def MergeCountSplitInv(C, D):
+# merge and count split inversions subroutine
+def MergeCountSplitInv(C, D, merged):
     """complexity O(n)"""
     # initialise indices
     i = 0
     j = 0
+    n = len(C) + len(D)
     splitInv = 0
-    while i < len(left) and j < len(right):
-        if left[i] <= right[j]:
-            merged[i+j] = left[i]
+    while i < len(C) and j < len(D):
+        if C[i] <= D[j]:
+            merged[i + j] = C[i]
             i += 1
         else:
-            merged[i+j] = right[j]
+            merged[i + j] = D[j]
             j += 1
-            splitInv = splitInv + (n/2 - i + 1)
+            splitInv = splitInv + (n / 2 - i + 1)
 
     # copy leftovers
-    for i in range(i, len(left)):
-        merged[i + j] = left[i]
+    for i in range(i, len(C)):
+        merged[i + j] = C[i]
 
-    for j in range(j, len(right)):
-        merged[i + j] = right[j]
-    return B, splitInv
+    for j in range(j, len(D)):
+        merged[i + j] = D[j]
+    return merged, splitInv
+
 
 # test
 if __name__ == '__main__':
-    SortCountInv(testArray)
+    with open("testArray.txt", "r") as file:
+        lines = file.readlines()  # lines is now a list with 100 000 elements
+        print(SortCountInv(lines))
+    file.close()
